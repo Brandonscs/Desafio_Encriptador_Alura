34265302fd161ecb02b472566899e08270f37dc3
Brandonscs <brandoncarvajal2002@gmail.com>
2024-08-09 20:50:36 -0500

    se reestructuro el index.html y se actualizo el app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-document.getElementById("texto__devuelto").style.display = "none";
+document.getElementById("mensaje-lleno").style.display = "none";
 
 function mostrarOcultarMensaje() {
-    let texto = document.getElementById("texto__ingresado").value;
+    let texto = document.getElementById("entrada-mensaje-area").value;
 
     if (texto.trim() === "") {
-        document.getElementById("texto__devuelto").style.display = "none";
-        document.getElementById("texto__vacio").style.display = "block";
+        document.getElementById("mensaje-lleno").style.display = "none";
+        document.getElementById("mensaje-vacio").style.display = "block";
     } else {
-        document.getElementById("texto__devuelto").style.display = "block";
-        document.getElementById("texto__vacio").style.display = "none";
+        document.getElementById("mensaje-lleno").style.display = "block";
+        document.getElementById("mensaje-vacio").style.display = "none";
     }
 }
